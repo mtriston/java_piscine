f4a4aefe4561988c2ce4153ae85758a139c2c991
Alexander Spivak <po4ta.polevaya@gmail.com>
2021-05-31 13:43:56 +0300

    Add two modes: dev and production

--- day01/ex05/Program.java
+++ day01/ex05/Program.java
@@ -1,6 +1,16 @@
 public class Program {
-    public static void main(String[] args) {
-        Menu menu = new Menu(ProfileType.DEV);
+    public static void main(String[] args)
+    {
+        Menu menu;
+        if (args.length == 0) {
+            menu = new Menu(ProfileType.PRODUCTION);
+
+        } else if (args.length == 1 && args[0].equals("-config=dev")) {
+            menu = new Menu(ProfileType.DEV);
+        } else {
+            System.err.println("Invalid argument. Program will start in production mode");
+            menu = new Menu(ProfileType.PRODUCTION);
+        }
         menu.start();
         menu.finish();
     }
