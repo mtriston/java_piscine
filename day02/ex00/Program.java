7994aeb09e2fe10e80368691262f027b438183ab
Alexander Spivak <po4ta.polevaya@gmail.com>
2021-06-02 16:50:32 +0300

    доделал парсер сигнатур, сделал конвертацию байтов в строку

--- day02/ex00/Program.java
+++ day02/ex00/Program.java
@@ -1,6 +1,26 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Program {
 	public static void main(String[] args) {
-		SignatureChecker signatureChecker = new SignatureChecker("result.txt");
+		SignatureChecker signatureChecker = new SignatureChecker("signatures.txt");
+	
 		
+
+		try (InputStream inputStream = new FileInputStream("picture.png")) {
+			byte []buf = new byte[8];
+			inputStream.read(buf);
+			String magicNumbers = new String();
+			for (byte b : buf) {
+				String oneByte = new String(Integer.toHexString(Byte.toUnsignedInt(b)));
+				if (oneByte.length() == 1) {
+					oneByte = "0" + oneByte;
+				}
+				magicNumbers += oneByte.toUpperCase();
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
--- day02/ex00/SignatureChecker.java
+++ day02/ex00/SignatureChecker.java
@@ -25,19 +25,17 @@ public class SignatureChecker {
 		try (InputStream inputStream = new FileInputStream(file)) {
 			byte []buf = new byte[inputStream.available()];
 			inputStream.read(buf);
-			for (int i = 0; i < buf.length; ++i) {
-				System.out.print(buf[i]);
+			String text = new String(buf);
+			String []lines = text.split("\n");
+			for (String line : lines) {
+				String []str = line.split(", ");
+				if (str.length != 2) {
+					System.err.printf("Invalid %s\n", file);
+					System.exit(-1);
+				}
+				str[1] = str[1].replaceAll("\\s", "");
+				this.signatures.put(str[1], str[0]);
 			}
-			// String text = new String(buf);
-			// String []lines = text.split("\n");
-			// for (String line : lines) {
-			// 	System.out.println(line);
-			// 	String []str = line.split(", ");
-			// 	if (str.length != 2) {
-			// 		System.err.printf("Invalid %s\n", file);
-			// 		System.exit(-1);
-			// 	}
-			// 	this.signatures.put(str[1], str[0]);
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
