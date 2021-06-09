6ff69ba1bba5a5f0264766848d3485c4bcf74989
Alexander Spivak <po4ta.polevaya@gmail.com>
2021-06-09 16:50:34 +0300

    нужноо тестить

---
+++ day03/ex02/Counter.java
@@ -0,0 +1,32 @@
+public class Counter implements Runnable {
+
+	private final int index;
+	private final int start;
+	private final int end;
+	private final int[] array;
+
+	Counter(int index, int span, int[] array) {
+		this.index = index;
+		this.start = index * span;
+
+		if (start + span > array.length) {
+			this.end = array.length;
+		} else {
+			this.end = start + span;
+		}
+		
+		this.array = array;
+	}
+
+	@Override
+	public void run() {
+		int sum = 0;
+		for (int i = start; i < end; ++i) {
+			sum += array[i];
+		}
+		System.out.printf("Thread %d: from %d to %d sum is %d\n",
+			index, start, end, sum);
+		Program.sumByThreads += sum;
+	}
+	
+}
---
+++ day03/ex02/Program.java
@@ -0,0 +1,68 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Program {
+
+	public static volatile int sumByThreads = 0;
+	public static void main(String[] args) {
+
+		int []array;
+		int threadsCount = 0;
+		int arraySize = 0;
+		int span;
+
+		if (args.length != 2) {
+			System.err.println("Invalid number of arguments!");
+			System.err.println("Example: java Program --arraySize=13 --threadsCount=3");
+			System.exit(-1);
+		}
+
+		if (args[0].substring(0, 12).compareToIgnoreCase("--arraySize=") != 0 ||
+			args[1].substring(0, 15).compareToIgnoreCase("--threadsCount=") != 0)
+		{
+			System.err.println("Invalid format of arguments!");
+			System.err.println("Example: java Program --arraySize=13 --threadsCount=3");
+			System.exit(-1);
+		}
+
+		try {
+			threadsCount = Integer.parseInt(args[0].substring(12));
+			arraySize = Integer.parseInt(args[1].substring(15));
+		} catch (NumberFormatException e) {
+			System.err.println(e.getMessage());
+			System.err.println("Example: java Program --arraySize=13 --threadsCount=3");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+
+		span = (int)Math.ceil((double)arraySize / threadsCount);
+
+		array = new Random().ints(arraySize, -1000, 1000).toArray();
+
+		int sum = 0;
+		for (int element : array) {
+			sum += element;
+		}
+		System.out.printf("Sum: %d\n", sum);
+
+		List<Thread> threads = new ArrayList<Thread>();
+
+		for (int i = 0; i < threadsCount; ++i) {
+			threads.add(new Thread(new Counter(i, span, array)));
+			threads.get(i).start();
+		}
+		
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				System.err.println(e.getMessage());
+				e.printStackTrace();
+			}
+			
+		}
+
+		System.out.printf("Sum by threads: %d\n", sumByThreads);
+	}
+}
