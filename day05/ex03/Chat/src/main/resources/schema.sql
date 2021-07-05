d311db168a9a7c82c6e2580f6576bc5b1b42cfec
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-07-05 22:13:15 +0300

    ex03 done

---
+++ day05/ex03/Chat/src/main/resources/schema.sql
@@ -0,0 +1,23 @@
+drop schema if exists chat cascade;
+
+create schema if not exists chat;
+
+create table if not exists chat.users (
+     id serial primary key,
+     login text unique not null,
+     password text not null
+);
+
+create table if not exists chat.rooms (
+    id serial primary key,
+    name text unique not null,
+    owner int references chat.users(id) not null
+);
+
+create table if not exists chat.messages (
+    id serial primary key,
+    author int references chat.users(id),
+    room int references chat.rooms(id),
+    text text,
+    date TIMESTAMP
+);
