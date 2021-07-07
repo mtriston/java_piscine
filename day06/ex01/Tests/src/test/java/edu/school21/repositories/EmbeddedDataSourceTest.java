12ac9899622d7cb4d1f87dba7fdabde5415543a4
Spivak Alexander <po4ta.polevaya@gmail.com>
2021-07-07 21:36:36 +0300

    Добавлен метод testConnection

--- day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
+++ day06/ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -2,13 +2,17 @@ package edu.school21.repositories;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class EmbeddedDataSourceTest {
+
+    private Connection connection;
 
     @BeforeEach
     public void init() throws SQLException {
@@ -17,6 +21,11 @@ public class EmbeddedDataSourceTest {
                 .addScript("schema.sql")
                 .addScript("data.sql")
                 .build();
-        Assertions.assertNotNull(db.getConnection());
+        this.connection = db.getConnection();
+    }
+
+    @Test
+    public void testConnection() {
+        Assertions.assertNotNull(connection);
     }
 }
